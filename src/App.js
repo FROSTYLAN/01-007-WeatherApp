0db17e3e675578bd10398ce5846da1fe8d3c027b
FROSTYLAN <castillo089frosty@gmail.com>
2022-03-07 13:04:41 -0500

    conexion a la api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [weather, setWeather] = useState();
+
+  const success = pos => {
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longitude;
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2b6fb305fce3d2641512ec2c4368693f`)
+      .then(res => (setWeather(res)))
+  }
+
+  useEffect(() => {
+     navigator.geolocation.getCurrentPosition(success);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={`http://openweathermap.org/img/wn/${weather?.weather?.[0]?.icon}@2x.png`} alt="" />
     </div>
   );
 }
